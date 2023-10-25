66c6be3dff25c1982b47ce8bba97fce31c73341c
almikoltc <almikoltc@yandex.ru>
2023-10-25 09:40:23 +0500

    численность по закрепляемости рефакт

--- index.js
+++ index.js
@@ -131,7 +131,6 @@ questions: {
               {
                 indictors.forEach(indicator =>
                 {
-
                   [null, '1-2023', '2-2023', '3-2023', '4-2023', '5-2023', '6-2023', '7-2023', '8-2023', '9-2023', '10-2023', '11-2023', '12-2023'].forEach(monthGroup =>
                   {
                     allQuestionsHead.push({
